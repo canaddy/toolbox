086b095e069eade27651fc6bec54783c529a4749
canaddy <canaddy_2007@126.com>
2016-11-23 20:27:29 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var path = require('path');
+var gulp = require('gulp');
+var babel = require("gulp-babel");
+var gutil = require('gulp-util');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
+var electron = require('electron-connect').server.create();
+
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BACKEND_PATH = path.resolve(APP_PATH, 'backend');
+var CLIENT_PATH = path.resolve(APP_PATH, 'client');
+
+
+// 开发
+var webpackConfigDev = Object.create(webpackConfig);
+webpackConfigDev.devtool = 'eval-source-map';
+webpackConfigDev.debug = true;
+
+var devCompiler = webpack(webpackConfigDev);
+
+// renderer process 的 webpack 编译
+gulp.task('webpack:build-dev', function () {
+  devCompiler.run(function (err, status) {
+    if (err) {
+      throw new gutil.PluginError('webpack:build-dev', err);
+    }
+    gutil.log('[webpack:build-dev]', status.toString({
+      colors: true
+    }));
+  });
+});
+
+// main process 的编译
+gulp.task('babel:electron-main', function () {
+  return gulp.src([APP_PATH + '/main.js', APP_PATH + '/config.js', BACKEND_PATH + '/**/*.{json,js}', APP_PATH + '/common/**/*.js'], { base: APP_PATH })
+    .pipe(babel())
+    .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('watch', ['babel:electron-main', 'webpack:build-dev'], function () {
+
+  electron.start();
+  gulp.watch(['./dist/main.js'], electron.restart);
+  gulp.watch(['./dist/client/*.{html,js,less,css}', './dist/client/**/*.{html,js,less,css}'], electron.reload);
+});
+
+gulp.task('watch:build', function(){
+  gulp.watch([CLIENT_PATH + '/**/*.{html,js,less,css}'], ['webpack:build-dev']);
+  gulp.watch([APP_PATH + '/main.js', BACKEND_PATH + '/**/*.js'], ['babel:electron-main']);
+});
+
+
+gulp.task('dev', ['watch:build', 'watch']);
